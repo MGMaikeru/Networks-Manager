66675c9b2f83896ae69746f9029478c12b0cd0c0
MGMaikeru <13miguegonza2005@gmail.com>
2023-05-25 12:32:27 -0500

    Upload priority queue

--- src/model/GraphForMatrix.java
+++ src/model/GraphForMatrix.java
@@ -1,13 +1,16 @@
 package model;
 
 
+import exception.EmptyFieldException;
+
 import java.util.ArrayList;
 
 public class GraphForMatrix <T, K extends Comparable<K>>{
     private int numVertices;
     private int[][] adjMatrix;
-    private ArrayList<Vertex1> vertices;
+    private ArrayList<Vertex1<T, K>> vertices;
     private boolean isDirected;
+    private int time;
 
     public GraphForMatrix (boolean isDirected) {
         this.isDirected = isDirected;
@@ -18,7 +21,7 @@ public class GraphForMatrix <T, K extends Comparable<K>>{
     }
 
     public void addVertex(K key, T value) {
-        Vertex1 vertex = new Vertex1(value, key);
+        Vertex1<T, K> vertex = new Vertex1<T, K>(value, key);
         int newIndex = vertices.size();
         numVertices ++;
         vertices.add(vertex);
@@ -33,12 +36,12 @@ public class GraphForMatrix <T, K extends Comparable<K>>{
     }
 
     public void addEdge(K srcKey, K destKey) {
-        Integer srcIdx = searchVertex(srcKey);
-        Integer destIdx = searchVertex(destKey);
+        int srcIdx = searchVertex(srcKey);
+        int destIdx = searchVertex(destKey);
 
         if (srcIdx != -1 && destIdx != -1) {
-            Vertex1 srcVertex = vertices.get(srcIdx);
-            Vertex1 destVertex = vertices.get(destIdx);
+            Vertex1<T, K> srcVertex = vertices.get(srcIdx);
+            Vertex1<T, K> destVertex = vertices.get(destIdx);
             srcVertex.addEdge(new Edge(srcVertex, destVertex, 1));
 
             if (isDirected){
@@ -60,12 +63,12 @@ public class GraphForMatrix <T, K extends Comparable<K>>{
     }
 
     public void removeEdge(K srcKey, K destKey) {
-        Integer srcIdx = searchVertex(srcKey);
-        Integer destIdx = searchVertex(destKey);
+        int srcIdx = searchVertex(srcKey);
+        int destIdx = searchVertex(destKey);
 
         if (srcIdx != -1 && destIdx != -1) {
-            Vertex1 srcVertex = vertices.get(srcIdx);
-            Vertex1 destVertex = vertices.get(destIdx);
+            Vertex1<T, K> srcVertex = vertices.get(srcIdx);
+            Vertex1<T, K> destVertex = vertices.get(destIdx);
             srcVertex.removeEdge(destVertex);
 
             if (isDirected){
@@ -85,5 +88,92 @@ public class GraphForMatrix <T, K extends Comparable<K>>{
         }
     }
 
+    public String bfs(K key) throws EmptyFieldException {
+        Vertex1<T, K> toSearch = vertices.get(searchVertex(key));
+        Queue<Vertex1<T, K>> queue= new Queue<>();
+        String msg = "";
+        if (toSearch == null){
+            return "This value don't exist in the graph";
+        }
+        for (Vertex1<T, K> vertex: vertices) {
+            vertex.setColor(0);
+            vertex.setPredecessor(null);
+            vertex.setDInit(0);
+        }
+        toSearch.setColor(1);
+        queue.enqueue(new Node1<>(toSearch));
+        while (!queue.isEmpty()){
+            Vertex1<T, K> u = queue.dequeue();
+
+            for (Edge e: u.getEdges()) {
+                Vertex1<T, K> v = e.getFinalVertex();
+                if (v.getColor() == 0) {
+                    v.setColor(1);
+                    v.setDInit(u.getDInit() + 1);
+                    v.setPredecessor(u);
+                    queue.enqueue(new Node1<>(v));
+                }
+            }
+            u.setColor(2);
+        }
+        for (Vertex1<T, K> vertex: vertices) {
+            msg += vertex.getValue() + " " + vertex.getDInit() + " away.\n";
+        }
+        return msg;
+    }
+
+    public void dfs(){
+        String msg = "";
+        for (Vertex1<T, K> vertex: vertices) {
+            vertex.setColor(0);
+            vertex.setPredecessor(null);
+            vertex.setDInit(0);
+
+        }
+        this.time = 0;
+        for (Vertex1<T, K> vertex: vertices) {
+            if (vertex.getColor() == 0)
+                dfs(vertex);
+        }
+    }
+
+    private void dfs(Vertex1<T, K> vertex){
+        time ++;
+        vertex.setDInit(time);
+        vertex.setColor(1);
+        for (Edge edge: vertex.getEdges()) {
+            Vertex1<T, K> v = edge.getFinalVertex();
+            if (v.getColor() == 0) {
+                v.setPredecessor(vertex);
+                dfs(v);
+            }
+        }
+        vertex.setColor(2);
+        time++;
+        vertex.setDEnd(time);
+    }
+
+    public String checkDfs(){
+        String msg = "";
+        for (Vertex1<T, K> vertex: vertices) {
+            msg += vertex.getValue() + " d: " + vertex.getDInit() + " f: " + vertex.getDEnd() + "\n";
+        }
+        return msg;
+    }
+
+    public void floyd(){
+        int[][] floydMatrix = new int[numVertices][numVertices];
+        for (int i = 0; i<numVertices; i++){
+            for (int j = 0; i<numVertices; i++){
+                floydMatrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+    }
+
+    public void dijkstra(K key){
+        int index = searchVertex(key);
+        vertices.get(index);
+
+    }
 
 }
---
+++ src/model/Heap.java
@@ -0,0 +1,141 @@
+package model;
+
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Heap<T extends Comparable<T>> extends IPriorityQueue<T> {
+
+    private int heapSize;
+    private ArrayList<T> array;
+
+    public Heap(){
+        this.array = new ArrayList<>();
+    }
+
+    public void insertElement(T object){
+     this.array.add(object);
+    }
+
+    /*@Override
+    public T heapMaximum(){
+        return array.get(0);
+    }*/
+
+    @Override
+    public T heapMinimum(){
+        return array.get(0);
+    }
+
+    public String printArray(){
+        String list = "";
+        for (int i = 0; i<array.size();i++){
+            list += (i+1) + ") " +array.get(i).toString() + "\n";
+        }
+        return list;
+    }
+
+    public ArrayList<T> getArray(){
+        return array;
+    }
+
+    @Override
+    public T heapExtractMin(){
+        if (array.size() < 1) {
+            System.out.println("heap underflow");
+        }
+        T min = array.get(0);
+        array.set(0, array.get(array.size()-1));
+        array.remove(array.size()-1);
+        minHeapify(0);
+        return min;
+    }
+
+    @Override
+    public void heapIncreaseKey(int i, T key){
+        if (key.compareTo( array.get(i) ) < 0 ) {
+            System.out.println("New key is smaller than current key");
+        }
+        array.set(i, key);
+
+        while (i > 0 && array.get( (int)father(i) ).compareTo(array.get(i)) < 0){
+            Collections.swap(array, i, (int)father(i));
+            i = (int)father(i);
+        }
+    }
+
+    @Override
+    public void minHeapInsert(T key){
+        array.add(key);
+        heapIncreaseKey(array.size()-1, key);
+    }
+
+    public void heapSort(){
+        buildMaxHeap();
+        for (int i = array.size(); i>=1; i--) {
+            Collections.swap(array, 0, i - 1);
+            reduceHeapSize();
+            minHeapify(0);
+        }
+    }
+
+    public void buildMaxHeap(){
+        this.heapSize = array.size()-1;
+        for (int i = (int)Math.floor(array.size()/2); i>=1; i--){
+            minHeapify(i-1);
+        }
+    }
+
+    public void reduceHeapSize(){
+        this.heapSize--;
+    }
+
+    public void minHeapify(int index){
+        int l = left(index);
+        int r = right(index);
+        int shortest = 0;
+
+        if (l >= heapSize && array.get(l).compareTo(array.get(index))< 0) {
+            shortest = l;
+        }else{
+            shortest = index;
+        }
+
+        if (r >= heapSize && array.get(r).compareTo(array.get(shortest))< 0) {
+            shortest = r;
+        }
+        if (shortest != index) {
+            Collections.swap(array, index, shortest);
+            minHeapify(shortest);
+        }
+        return;
+    }
+
+    public int getHeapSize(){
+        int size = 0;
+        for (int i = array.size();i>= 0; i--){
+            if (father(i) != 0) {
+                if (array.get((int)father(i)-1).compareTo(array.get(i-1)) >= 0){
+                    size += 1;
+                } else{
+                    size = 0;
+                }
+            }
+
+        }
+        return size+1;
+    }
+
+    public int right(int index){
+        return index*2+2;
+    }
+
+    public int left(int index){
+        return index*2+1;
+    }
+
+    public double father(int index){
+        return Math.floor(index/2);
+    }
+
+
+}
